080934a5a6f6d03b71f2b19710b13eba6603527a
ArefAH <arefabouhamdan@gmail.com>
2024-11-11 01:09:45 +0200

    PHP - Added connection file

---
+++ backend/connection.php
@@ -0,0 +1,17 @@
+<?php
+
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, GET");
+
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'cinematch';
+
+$connection = new mysqli($host, $username, $password, $dbname);
+
+if($connection -> connect_error){
+    die('Connection Error');
+}
+?>
