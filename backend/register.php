097a3cbfcc0ee27e5cdd029fb36e6bad6fc933cd
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-11-11 12:53:20 +0200

    PHP <> checked if no users already exist to assigne the first user registered as admin

--- backend/register.php
+++ backend/register.php
@@ -9,6 +9,20 @@ include "connection.php";
 $username = $_POST["username"];
 $password = $_POST["password"];
 
+// Check if this is the first user being added
+$check_user_count_query = $connection->prepare("SELECT COUNT(*) AS user_count FROM users");
+$check_user_count_query->execute();
+$result = $check_user_count_query->get_result();
+$row = $result->fetch_assoc();
+
+if ($row['user_count'] == 0) {
+    // Assign user_type_id as 1 for the first user (aka admin)
+    $user_type_id = 1;
+} else {
+    // Assign user_type_id as 2 for all other users (aka regular users)
+    $user_type_id = 2;
+}
+
 $hashed = password_hash($password, PASSWORD_DEFAULT);
 
 $query = $connection->prepare("INSERT INTO users (username, password, user_type_id) VALUES (?, ?, ?)");
