26625940068f7e5b24f2efd706f1f9efd71bffef
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-11-11 15:17:10 +0200

    JS <> added login functionalities

--- frontend/scripts/authentication.js
+++ frontend/scripts/authentication.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const loginForm = document.getElementById("loginForm");
-    const registerForm = document.getElementById("registerForm");
-    const registerBtn = document.getElementById("registerBtn");
-    const backLoginBtn = document.getElementById("back-login-Btn");
+    const loginForm = document.getElementById("login-Form");
+    const registerForm = document.getElementById("register-Form");
+    const registerBtn = document.getElementById("register-btn");
+    const backLoginBtn = document.getElementById("back-login-btn");
 
     if (loginForm) {
         loginForm.addEventListener("submit", async function (event) {
@@ -11,22 +11,9 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    if (registerForm) {
-        registerForm.addEventListener("submit", async function (event) {
-            event.preventDefault();
-            handleRegister();
-        });
-    }
-
     if (registerBtn) {
         registerBtn.addEventListener("click", function () {
-            window.location.href = "pages/register.html";
-        });
-    }
-    if (backLoginBtn) {
-        backLoginBtn.addEventListener("click", function () {
-            window.location.href = "../index.html";
-            console.log("Back to login");
+            window.location.href = "register.html";
         });
     }
 });
@@ -34,3 +21,38 @@ document.addEventListener("DOMContentLoaded", function () {
 let username;
 let password;
 
+
+async function handleLogin() {
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+
+    axios({
+        method: "post",
+        url: "http://localhost/cinematch/backend/login.php",
+        data: new URLSearchParams({
+            username: username,
+            password: password,
+        }),
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+    })
+        .then((response) => {
+            console.log(response.data);
+
+            if (response.data.status === "Login Successful") {
+                localStorage.setItem("username", response.data.username);
+                localStorage.setItem("user_id", response.data.user_id);
+                alert("Login successful! Redirecting...");
+                // window.location.href = "/pages/home.html"; 
+                console.log("worked");
+
+            } else {
+                document.getElementById("incorrect-password").innerHTML = '<p class="message-red">Invalid username or password.</p>'
+            }
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+            alert("An error occurred. Please try again.");
+        });
+};
