3e827376e1bf206998135ed4069a3bfe1366d21a
ArefAH <arefabouhamdan@gmail.com>
2024-11-13 00:08:52 +0200

    JavaScript - Added element pointer

---
+++ frontend/scripts/dom.js
@@ -0,0 +1,7 @@
+const poster = document.getElementById('poster');
+const title = document.getElementById('title');
+const duration = document.getElementById('duration');
+const genre = document.getElementById('genre');
+const release = document.getElementById('release');
+const rating = document.getElementById('rating');
+const description = document.getElementById('description');
