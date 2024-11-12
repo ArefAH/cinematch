e04424423bd226f76f1bcd4ff9178b3b47ff2631
ArefAH <arefabouhamdan@gmail.com>
2024-11-13 01:07:00 +0200

    JavaScript - Added getInfo function to fetch movie info

---
+++ frontend/scripts/description.js
@@ -0,0 +1,21 @@
+const userId = window.localStorage.getItem("userId");
+const movieId = window.localStorage.getItem("movieId");
+
+const instance = axios.create({
+  baseURL: "https://localhost/cinematch/backend/",
+});
+
+const getInfo = async () => {
+  const response = await instance.post("/getInfo.php", {
+    userId: userId,
+    movieId: movieId,
+  });
+  const data = response.data;
+  poster.src = data.image;
+  title.textContent = data.title;
+  duration.textContent = data.duration;
+  genre.textContent = data.genre;
+  description.textContent = data.description;
+};
+
+getInfo();
