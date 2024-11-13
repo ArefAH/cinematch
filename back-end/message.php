8db129632d60d986c60172479b285c12eb862b06
gabykassab00 <gabykassab0@gmail.com>
2024-11-13 20:21:00 +0200

    creating replies to test

--- back-end/message.php
+++ back-end/message.php
@@ -4,3 +4,14 @@ $conn = mysqli_connect("localhost", "root", "", "q");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+$getMesg = $_POST['text'];
+$query = "SELECT replies FROM qq WHERE queries LIKE CONCAT('%', ?, '%')";
+$stmt = mysqli_prepare($conn, $query);
+
+mysqli_stmt_bind_param($stmt, "s", $getMesg);
+
+mysqli_stmt_execute($stmt);
+
+$result = mysqli_stmt_get_result($stmt);
+
