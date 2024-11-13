e4ce220be1c42801797bb385ca48eb07eaf7da65
gabykassab00 <gabykassab0@gmail.com>
2024-11-13 20:22:04 +0200

    creating an api key with the url

--- back-end/message.php
+++ back-end/message.php
@@ -15,3 +15,23 @@ mysqli_stmt_execute($stmt);
 
 $result = mysqli_stmt_get_result($stmt);
 
+if (mysqli_num_rows($result) > 0) {
+    $fetch_data = mysqli_fetch_assoc($result);
+    echo $fetch_data['replies'];
+} else {
+    $apiKey = "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";
+    $apiUrl = "https://api.openai.com/v1/chat/completions"; 
+
+    $data = array(
+        "model" => "gpt-3.5-turbo", 
+        "messages" => [
+            ["role" => "system", "content" => "You are a helpful assistant."],
+            ["role" => "user", "content" => $getMesg] 
+        ]
+    );
+
+}
+
+mysqli_stmt_close($stmt);
+mysqli_close($conn);
+?>
