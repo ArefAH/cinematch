b7d50d264301dd7310922109f04a9a9571320ea8
ArefAH <arefabouhamdan@gmail.com>
2024-11-13 12:27:23 +0200

    JavaScript - Added updateStar function the changes stars from empty to filled when pressed

--- frontend/scripts/rating.js
+++ frontend/scripts/rating.js
@@ -2,6 +2,14 @@ stars.forEach(star => {
     star.addEventListener('click', () => {
         const value = star.getAttribute('data-value');
         ratingValue.textContent = `${value}`;
+        updateStar(value);
     });
 
 });
+
+function updateStar(value) {
+    stars.forEach(star => {
+        const starValue = star.getAttribute('data-value');
+        star.src = starValue <= value ? './../assets/icons/star.svg' : './../assets/icons/star-empty.svg';
+    });
+}
